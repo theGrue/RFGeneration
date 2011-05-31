0804dc7f3a7c175a89bf204c88edf75fc399ff41
theGrue <gruenberg@gmail.com>
2011-05-30 23:27:08 -0400

    Removed some unused resources.

--- src/com/rfgeneration/GameInfoActivity.java
+++ src/com/rfgeneration/GameInfoActivity.java
@@ -19,7 +19,6 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.LinearLayout;
 import android.widget.TableRow.LayoutParams;
