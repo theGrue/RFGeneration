eb5b98ac2b9eabe422bc501a5e1e36a7a055446b
theGrue <gruenberg@gmail.com>
2011-11-13 22:19:55 -0500

    Added error screen when game info doesn't load properly.

--- src/com/jgrue/rfgeneration/GameInfoActivity.java
+++ src/com/jgrue/rfgeneration/GameInfoActivity.java
@@ -22,6 +22,7 @@ import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
@@ -70,121 +71,140 @@ public class GameInfoActivity extends Activity implements OnClickListener {
 	            
 	            try {
 	    			gameInfo = GameInfoScraper.getGameInfo(getIntent().getStringExtra("GAMEINFO_RFGID"));
-	    		} catch (Exception e) { }
 	    		
-	    		// Detail
-	    		TextView console = (TextView) findViewById(R.id.gameDetailConsole);
-	            if (console != null)
-	            	console.setText(gameInfo.getConsole());
-	            
-	            TextView title = (TextView) findViewById(R.id.gameDetailTitle);
-	            if(title != null)
-	            {
-	            	title.setText(Html.fromHtml("<b>" + gameInfo.getTitle() + "</b>"));
-	            	if(gameInfo.getVariationTitle() != null)
-	               		title.setText(Html.fromHtml("<b>" + gameInfo.getTitle() + "</b> [" + gameInfo.getVariationTitle() + "]"));
-	            }
-	            
-	            TextView alternateTitle = (TextView) findViewById(R.id.gameDetailAlternateTitle);
-	            if(alternateTitle != null)
-	            	alternateTitle.setText(gameInfo.getAlternateTitle());
-
-	            TextView year = (TextView) findViewById(R.id.gameDetailYear);
-	            if(year != null)
-	            	year.setText("" + gameInfo.getYear());
-	            
-	            TextView rfgId = (TextView) findViewById(R.id.gameDetailRFGID);
-	            if(rfgId != null)
-	            	rfgId.setText(gameInfo.getRFGID());
-	            
-	            TextView part = (TextView) findViewById(R.id.gameDetailPart);
-	            if(part != null)
-	            	part.setText(gameInfo.getPartNumber());
-	            
-	            TextView upc = (TextView) findViewById(R.id.gameDetailUPC);
-	            if(upc != null)
-	            {
-	            	if(!gameInfo.getUPC().equals("")) {
-	            		upc.setText(Html.fromHtml(gameInfo.getUPC() + " <a href=\"http://videogames.pricecharting.com/search/?q=" + gameInfo.getUPC() + "\">VGPC</a>"));
-	            		upc.setMovementMethod(LinkMovementMethod.getInstance());
-	            	} else {
-	            		upc.setText(gameInfo.getUPC());
-	            	}
-	            }
-
-	            TextView publisher = (TextView) findViewById(R.id.gameDetailPublisher);
-	            if(publisher != null)
-	            	publisher.setText(gameInfo.getPublisher());
-	            
-	            TextView developer = (TextView) findViewById(R.id.gameDetailDeveloper);
-	            if(developer != null)
-	            	developer.setText(gameInfo.getDeveloper());
-	            
-	            TextView rating = (TextView) findViewById(R.id.gameDetailRating);
-	            if(rating != null)
-	            	rating.setText(gameInfo.getRating());
-	            
-	            TextView genre = (TextView) findViewById(R.id.gameDetailGenre);
-	            if(genre != null)
-	            	genre.setText(gameInfo.getGenre());
-	            
-	            TextView subgenre = (TextView) findViewById(R.id.gameDetailSubgenre);
-	            if(subgenre != null)
-	            	subgenre.setText(gameInfo.getSubGenre());
-	            
-	            TextView players = (TextView) findViewById(R.id.gameDetailPlayers);
-	            if(players != null)
-	            	players.setText(gameInfo.getPlayers());
-	            
-	            TextView controller = (TextView) findViewById(R.id.gameDetailController);
-	            if(controller != null)
-	            	controller.setText(gameInfo.getControlScheme());
-	            
-	            TextView mediaFormat = (TextView) findViewById(R.id.gameDetailMediaFormat);
-	            if(mediaFormat != null)
-	            	mediaFormat.setText(gameInfo.getMediaFormat());
-	            
-	            ImageView region = (ImageView) findViewById(R.id.gameDetailRegion);
-	            if(region != null) {
-	            	if(gameInfo.getRegion().indexOf(",") == -1) {
-                		region.setImageDrawable(gameInfo.getRegionDrawable(region.getContext()));
-                	} else {                       		
-                		region.setImageDrawable(gameInfo.getRegionAnimation(region.getContext()));
-                	}
-	            }
-	            
-	            // Credits
-	            LinearLayout ll = (LinearLayout)findViewById(R.id.gameCreditsLayout);
-	            
-	            for(int i = 0; i < gameInfo.getNameList().size() && i < gameInfo.getCreditList().size(); i++) {
-	                TextView name = new TextView(GameInfoActivity.this);
-	                name.setText(gameInfo.getNameList().get(i));
-	                name.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
-	                name.setTextColor(0xffcc0000);
-	                name.setLayoutParams(new LayoutParams(
-	                		android.view.ViewGroup.LayoutParams.FILL_PARENT,
-	                		android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-	                ll.addView(name);
-	                
-	                TextView credit = new TextView(GameInfoActivity.this);
-	                credit.setText(gameInfo.getCreditList().get(i));
-	                credit.setTextColor(Color.BLACK);
-	                credit.setLayoutParams(new LayoutParams(
-	                		android.view.ViewGroup.LayoutParams.FILL_PARENT,
-	                		android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
-	                credit.setPadding((int)getResources().getDisplayMetrics().density * 10, 0, 0, (int)getResources().getDisplayMetrics().density * 5);
-	                ll.addView(credit);
-	            }
-	            
-	            ((TextView)findViewById(R.id.DetailDetail)).setOnClickListener(GameInfoActivity.this);
-	            ((TextView)findViewById(R.id.DetailImages)).setOnClickListener(GameInfoActivity.this);
-	            ((TextView)findViewById(R.id.DetailCredits)).setOnClickListener(GameInfoActivity.this);
-	            ((ImageView)findViewById(R.id.gameImagesLeft)).setOnClickListener(GameInfoActivity.this);
-	            ((ImageView)findViewById(R.id.gameImagesRight)).setOnClickListener(GameInfoActivity.this);
-	            
-	            try {
-	    			setCurrentLayout(findViewById(R.id.DetailDetail));
-	    		} catch (Exception e) { }
+		    		// Detail
+		    		TextView console = (TextView) findViewById(R.id.gameDetailConsole);
+		            if (console != null)
+		            	console.setText(gameInfo.getConsole());
+		            
+		            TextView title = (TextView) findViewById(R.id.gameDetailTitle);
+		            if(title != null)
+		            {
+		            	title.setText(Html.fromHtml("<b>" + gameInfo.getTitle() + "</b>"));
+		            	if(gameInfo.getVariationTitle() != null)
+		               		title.setText(Html.fromHtml("<b>" + gameInfo.getTitle() + "</b> [" + gameInfo.getVariationTitle() + "]"));
+		            }
+		            
+		            TextView alternateTitle = (TextView) findViewById(R.id.gameDetailAlternateTitle);
+		            if(alternateTitle != null)
+		            	alternateTitle.setText(gameInfo.getAlternateTitle());
+	
+		            TextView year = (TextView) findViewById(R.id.gameDetailYear);
+		            if(year != null)
+		            	year.setText("" + gameInfo.getYear());
+		            
+		            TextView rfgId = (TextView) findViewById(R.id.gameDetailRFGID);
+		            if(rfgId != null)
+		            	rfgId.setText(gameInfo.getRFGID());
+		            
+		            TextView part = (TextView) findViewById(R.id.gameDetailPart);
+		            if(part != null)
+		            	part.setText(gameInfo.getPartNumber());
+		            
+		            TextView upc = (TextView) findViewById(R.id.gameDetailUPC);
+		            if(upc != null)
+		            {
+		            	if(!gameInfo.getUPC().equals("")) {
+		            		upc.setText(Html.fromHtml(gameInfo.getUPC() + " <a href=\"http://videogames.pricecharting.com/search/?q=" + gameInfo.getUPC() + "\">VGPC</a>"));
+		            		upc.setMovementMethod(LinkMovementMethod.getInstance());
+		            	} else {
+		            		upc.setText(gameInfo.getUPC());
+		            	}
+		            }
+	
+		            TextView publisher = (TextView) findViewById(R.id.gameDetailPublisher);
+		            if(publisher != null)
+		            	publisher.setText(gameInfo.getPublisher());
+		            
+		            TextView developer = (TextView) findViewById(R.id.gameDetailDeveloper);
+		            if(developer != null)
+		            	developer.setText(gameInfo.getDeveloper());
+		            
+		            TextView rating = (TextView) findViewById(R.id.gameDetailRating);
+		            if(rating != null)
+		            	rating.setText(gameInfo.getRating());
+		            
+		            TextView genre = (TextView) findViewById(R.id.gameDetailGenre);
+		            if(genre != null)
+		            	genre.setText(gameInfo.getGenre());
+		            
+		            TextView subgenre = (TextView) findViewById(R.id.gameDetailSubgenre);
+		            if(subgenre != null)
+		            	subgenre.setText(gameInfo.getSubGenre());
+		            
+		            TextView players = (TextView) findViewById(R.id.gameDetailPlayers);
+		            if(players != null)
+		            	players.setText(gameInfo.getPlayers());
+		            
+		            TextView controller = (TextView) findViewById(R.id.gameDetailController);
+		            if(controller != null)
+		            	controller.setText(gameInfo.getControlScheme());
+		            
+		            TextView mediaFormat = (TextView) findViewById(R.id.gameDetailMediaFormat);
+		            if(mediaFormat != null)
+		            	mediaFormat.setText(gameInfo.getMediaFormat());
+		            
+		            ImageView region = (ImageView) findViewById(R.id.gameDetailRegion);
+		            if(region != null) {
+		            	if(gameInfo.getRegion().indexOf(",") == -1) {
+	                		region.setImageDrawable(gameInfo.getRegionDrawable(region.getContext()));
+	                	} else {                       		
+	                		region.setImageDrawable(gameInfo.getRegionAnimation(region.getContext()));
+	                	}
+		            }
+		            
+		            // Credits
+		            LinearLayout ll = (LinearLayout)findViewById(R.id.gameCreditsLayout);
+		            
+		            for(int i = 0; i < gameInfo.getNameList().size() && i < gameInfo.getCreditList().size(); i++) {
+		                TextView name = new TextView(GameInfoActivity.this);
+		                name.setText(gameInfo.getNameList().get(i));
+		                name.setTypeface(Typeface.SANS_SERIF, Typeface.BOLD);
+		                name.setTextColor(0xffcc0000);
+		                name.setLayoutParams(new LayoutParams(
+		                		android.view.ViewGroup.LayoutParams.FILL_PARENT,
+		                		android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
+		                ll.addView(name);
+		                
+		                TextView credit = new TextView(GameInfoActivity.this);
+		                credit.setText(gameInfo.getCreditList().get(i));
+		                credit.setTextColor(Color.BLACK);
+		                credit.setLayoutParams(new LayoutParams(
+		                		android.view.ViewGroup.LayoutParams.FILL_PARENT,
+		                		android.view.ViewGroup.LayoutParams.WRAP_CONTENT));
+		                credit.setPadding((int)getResources().getDisplayMetrics().density * 10, 0, 0, (int)getResources().getDisplayMetrics().density * 5);
+		                ll.addView(credit);
+		            }
+		            
+		            ((TextView)findViewById(R.id.DetailDetail)).setOnClickListener(GameInfoActivity.this);
+		            ((TextView)findViewById(R.id.DetailImages)).setOnClickListener(GameInfoActivity.this);
+		            ((TextView)findViewById(R.id.DetailCredits)).setOnClickListener(GameInfoActivity.this);
+		            ((ImageView)findViewById(R.id.gameImagesLeft)).setOnClickListener(GameInfoActivity.this);
+		            ((ImageView)findViewById(R.id.gameImagesRight)).setOnClickListener(GameInfoActivity.this);
+		            
+		            try {
+	            	setCurrentLayout(findViewById(R.id.DetailDetail));
+	            	} catch (Exception e) { }
+		            
+	            } catch (Exception e) {
+	    			Log.e("GameInfoActivity", "Error occurred while loading RFGID " + getIntent().getStringExtra("GAMEINFO_RFGID"));
+	    			
+	    			TextView errorText = (TextView)findViewById(R.id.gameDetailError);
+	    			if(errorText != null)
+	    				errorText.setText("\n\nSorry, an error occurred while loading this game. " +
+	    						"Please e-mail android@jgrue.com and report that an issue " +
+	    						"occurred with the following RFGID:\n\n");
+	    			
+	    			EditText errorRFGID = (EditText)findViewById(R.id.gameDetailErrorRFGID);
+	    			if(errorRFGID != null)
+	    				errorRFGID.setText(getIntent().getStringExtra("GAMEINFO_RFGID").substring(8));
+	    			
+	    			((ScrollView) findViewById(R.id.gameDetailScrollView)).setVisibility(View.GONE);
+	    			((LinearLayout) findViewById(R.id.gameDetailLayout)).setVisibility(View.GONE);
+	    			((RelativeLayout) findViewById(R.id.gameImagesLayout)).setVisibility(View.GONE);
+	    			((LinearLayout) findViewById(R.id.gameCreditsLayout)).setVisibility(View.GONE);
+	    			((LinearLayout) findViewById(R.id.gameErrorLayout)).setVisibility(View.VISIBLE);
+	    		}
 	        }
 	      };
 
